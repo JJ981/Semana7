5cb88c99cee9342765644d5fb4a6b2e27ea14990
GAMER <GAMER@DESKTOP-SCA0ECD>
2024-06-04 11:33:18 -0300

    Agregar archivos de proyecto.

---
+++ Models/Movie.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Semana7.Models
+{
+    public partial class Movie
+    {
+        public int Id { get; set; }
+        public string? Genero { get; set; }
+        public decimal? Precio { get; set; }
+        public DateTime? Lanzamiento { get; set; }
+        public string? Titulo { get; set; }
+    }
+}
---
+++ Models/MoviesContext.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Semana7.Models
+{
+    public partial class MoviesContext : DbContext
+    {
+        public MoviesContext()
+        {
+        }
+
+        public MoviesContext(DbContextOptions<MoviesContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Movie> Movies { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("server=DESKTOP-SCA0ECD\\SQLEXPRESS; Database=Movies; integrated security=true");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Movie>(entity =>
+            {
+                entity.Property(e => e.Id).ValueGeneratedNever();
+
+                entity.Property(e => e.Genero)
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Lanzamiento).HasColumnType("date");
+
+                entity.Property(e => e.Precio).HasColumnType("decimal(10, 2)");
+
+                entity.Property(e => e.Titulo)
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
